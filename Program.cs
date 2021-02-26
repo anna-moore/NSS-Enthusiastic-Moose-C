44cce3ab25cc4e65c6ba9a5a301e35b2d58375c7
anna-moore <ammoore434@gmail.com>
2021-02-26 14:34:25 -0600

    phase nine: refactor to reduce redundancy

--- Program.cs
+++ Program.cs
@@ -13,63 +13,25 @@ void Main()
     MooseSays("I really am enthusiastic");
 
     // As a question
-    CanadaQuestion();
-    EnthusiasticQuestion();
-    LoveCSharpQuestion();
-    SecretQuestion();
+    Question("Is Canada real?", "Really? It seems very unlikely.", "I  K N E W  I T !!!");
+    Question("Are you enthusiastic?", "Yay!", "You should try it!");
+    Question("Do you love C# yet?", "Good job sucking up to your instructor!", "You will...oh, yes, you will...");
+    Question("Do you want to know a secret?", "ME TOO!!!! I love secrets...tell me one!", "Oh, no...secrets are the best, I love to share them!");
 }
 
-void CanadaQuestion()
+void Question(string question, string yesAnswer, string noAnswer)
 {
-    bool isTrue = MooseAsks("Is Canada real?");
+    bool isTrue = MooseAsks(question);
     if (isTrue)
     {
-        MooseSays("Really? It seems very unlikely.");
+        MooseSays(yesAnswer);
     }
     else
     {
-        MooseSays("I  K N E W  I T !!!");
+        MooseSays(noAnswer);
     }
 }
 
-void EnthusiasticQuestion()
-{
-    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
-    if (isEnthusiastic)
-    {
-        MooseSays("Yay!");
-    }
-    else
-    {
-        MooseSays("You should try it!");
-    }
-}
-
-void LoveCSharpQuestion()
-{
-    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
-    if (doesLoveCSharp)
-    {
-        MooseSays("Good job sucking up to your instructor!");
-    }
-    else
-    {
-        MooseSays("You will...oh, yes, you will...");
-    }
-}
-
-void SecretQuestion()
-{
-    bool wantsSecret = MooseAsks("Do you want to know a secret?");
-    if (wantsSecret)
-    {
-        MooseSays("ME TOO!!!! I love secrets...tell me one!");
-    }
-    else
-    {
-        MooseSays("Oh, no...secrets are the best, I love to share them!");
-    }
-}
 void MooseSays(string message)
 {
     Console.WriteLine($@"
